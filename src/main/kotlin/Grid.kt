5af54cc294afee88b30e83027f276e7204a41864
Niels Falk <niels@nielsfalk.de>
2019-02-22 16:41:17 +0100

    string as grid

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -1,12 +1,10 @@
-import Direction.Left
-
 data class Grid(
     val fields: Map<Position, Int>,
     val rowCount: Int = 4,
     val colCount: Int = 4
 ) {
-    val rows = (0..rowCount).map(::Row)
-    val cols = (0..colCount).map(::Col)
+    val rows = (0..rowCount-1).map(::Row)
+    val cols = (0..colCount-1).map(::Col)
 
     override fun toString(): String =
         rows.map { row ->
@@ -14,6 +12,25 @@ data class Grid(
                 fields[Position(row, col)]?.toString() ?: " "
             }.joinToString(separator = ",")
         }.joinToString(separator = "\n")
+}
+
+fun String.asGrid(): Grid {
+    val rowStrings = split("\n")
+    return Grid(
+        rowStrings
+            .withIndex()
+            .flatMap { (row, value) ->
+                value.split(",")
+                    .withIndex()
+                    .map { (col, value) ->
+                        value.toIntOrNull()
+                            ?.let {
+                                Position(Row(row), Col(col)) to it
+                            }
+                    }.filterNotNull()
+            }.toMap(),
+        rowCount = rowStrings.size,
+        colCount = rowStrings.first().split(",").size)
 }
 
 enum class Direction {
--- src/test/kotlin/GridTest.kt
+++ src/test/kotlin/GridTest.kt
@@ -1,17 +1,24 @@
 import org.amshove.kluent.shouldEqual
-import org.hamcrest.MatcherAssert
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
 
-class GridTest{
+class GridTest {
     @Test
     fun print() {
         Grid(mapOf(Position(Row(1), Col(2)) to 3)).toString() shouldEqual
-                " , , , , \n" +
-                " , ,3, , \n" +
-                " , , , , \n" +
-                " , , , , \n" +
-                " , , , , "
+                " , , , \n" +
+                " , ,3, \n" +
+                " , , , \n" +
+                " , , , "
+    }
+
+    @Test
+    fun `string as grid`() {
+        ("" +
+                " , , , \n" +
+                " , ,3, \n" +
+                " , , , \n" +
+                " , , , "
+                ).asGrid() shouldEqual Grid(mapOf(Position(Row(1), Col(2)) to 3))
     }
 }
+
